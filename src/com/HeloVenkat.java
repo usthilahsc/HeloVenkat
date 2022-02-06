24d691de111a7676bc311a822401fd6acf6793be
reddy <reddy@gmail.com>
2022-02-06 19:46:28 +0530

    first java project commit

---
+++ src/com/HeloVenkat.java
@@ -0,0 +1,10 @@
+package com;
+
+public class HeloVenkat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Helo Venkat Welcome to Java Programming");
+	}
+
+}
