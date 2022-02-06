d118f7d25e4a346231845ac1acdd0cec22ffab07
reddy <venkat.reddyneo@gmail.com>
2022-02-06 21:09:28 +0530

    second commit

--- src/com/HeloVenkat.java
+++ src/com/HeloVenkat.java
@@ -5,6 +5,7 @@ public class HeloVenkat {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Helo Venkat Welcome to Java Programming");
+        System.out.println("Awesome Java Programming!!!");
 	}
 
 }
